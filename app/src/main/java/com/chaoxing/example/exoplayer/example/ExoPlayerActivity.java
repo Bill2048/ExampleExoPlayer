6f06b71ba43d6030bcc6584352c68d0941b350c4
Bill2048 <huwei2048@gmail.com>
2017-08-29 19:48:26 +0800

    night

---
+++ app/src/main/java/com/chaoxing/example/exoplayer/example/ExoPlayerActivity.java
@@ -0,0 +1,21 @@
+package com.chaoxing.example.exoplayer.example;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.chaoxing.example.exoplayer.R;
+
+/**
+ * Created by HuWei on 2017/8/29.
+ */
+
+public class ExoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exo_player);
+    }
+
+}
